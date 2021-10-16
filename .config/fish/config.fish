e513df1c3dc734a74fe079362a03a48b3dd513e9
stkr <stkr@users.noreply.github.com>
2021-10-16 12:26:19 +0200

    Add fish binding for completing suggestion.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     alias dt='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
+    # Use ctrl+l to complete one word of autosuggestion
+    bind -M insert \cl 'forward-word'
 end
