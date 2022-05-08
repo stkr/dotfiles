bdaf21d9c551f73c5bf675e57f05d714f0132a81
stkr <stkr@users.noreply.github.com>
2022-05-08 14:29:16 +0200

    Show paste mode in statusline.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -139,6 +139,25 @@ require('lualine').setup {
     component_separators = '|',
     section_separators = '',
   },
+
+  extensions = {},
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { "filename" },
+    lualine_x = { "location" },
+    lualine_y = {},
+    lualine_z = {}
+  },
+  sections = {
+    lualine_a = { "mode", "vim.o.paste and 'PASTE' or ''" },
+    lualine_b = { "branch", "diff", "diagnostics" },
+    lualine_c = { "filename" },
+    lualine_x = { "encoding", "fileformat", "filetype" },
+    lualine_y = { "progress" },
+    lualine_z = { "location" }
+  },
+  tabline = {}
 }
 
 -- Use treesitter for folding
