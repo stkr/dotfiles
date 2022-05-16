cbd299e50bb588d5debb5f909022dd93cbf38c60
stkr <stkr@users.noreply.github.com>
2022-05-16 08:33:50 +0200

    Add missing utils.lua for helper functions for nvim config.

---
+++ .config/nvim/lua/utils.lua
@@ -0,0 +1,19 @@
+
+local utils = {}
+
+function utils.get_plugin_config_module(plugin_name)
+    -- Deal with plugins that have weird characters in their name (often a '.').
+    -- For these we only use the first letters
+    local first_part = string.gmatch(plugin_name, "%a+")()
+    local plugin_config_name = "config." .. first_part
+    local present, module = pcall(require, plugin_config_name)
+    if not present then
+        vim.notify("Failed to require plugin config module [" .. plugin_config_name .. "]")
+        return
+    end
+    return module
+end
+
+
+return utils
+
