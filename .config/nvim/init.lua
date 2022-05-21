e8abf0eba64478775d0d12cb924d94faf1893057
stkr <stkr@users.noreply.github.com>
2022-05-21 13:35:19 +0200

    Re-enable git-signs.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -403,16 +403,16 @@ vim.g.indent_blankline_filetype_exclude = { 'help', 'packer' }
 vim.g.indent_blankline_buftype_exclude = { 'terminal', 'nofile' }
 vim.g.indent_blankline_show_trailing_blankline_indent = false
 
--- -- Gitsigns
--- require('gitsigns').setup {
---   signs = {
---     add = { text = '+' },
---     change = { text = '~' },
---     delete = { text = '_' },
---     topdelete = { text = '‾' },
---     changedelete = { text = '~' },
---   },
--- }
+-- Gitsigns
+require('gitsigns').setup {
+  signs = {
+    add = { text = '+' },
+    change = { text = '~' },
+    delete = { text = '_' },
+    topdelete = { text = '‾' },
+    changedelete = { text = '~' },
+  },
+}
 
 
 -- vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers)
