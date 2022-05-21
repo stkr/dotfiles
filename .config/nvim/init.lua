ea34e7c38d85b529c17380d28b41650fab13dd9b
stkr <stkr@users.noreply.github.com>
2022-05-21 13:50:35 +0200

    Properly set up lsp capabilities.
    Note, this is a bit of a hack to overcome the dilemma of not wanting to
    load cmp just to get the right capabilities of cmp at a point in time
    where cmp itself is perhaps not (yet) needed.
    We duplicated some lines of code from a plugin here.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -505,9 +505,39 @@ local on_attach = function(_, bufnr)
   vim.api.nvim_create_user_command("Format", vim.lsp.buf.formatting, {})
 end
 
+-- This is a copy from cmp_nvim_lsp/init.lua. The rationale here is that in order 
+-- to use cmp_nvim_lsp one would need to require nvim-cmp in addition. However, we 
+-- definitely want to lazy-load nvim-cmp as it is the biggest contributor to 
+-- startuptime. This function is actually required for lsp confiuration and
+-- independent from nvim-cmp so it can be easily extracted.
+local if_nil = function(val, default)
+    if val == nil then return default end
+    return val
+end
+
+local function update_capabilities(capabilities, override)
+    override = override or {}
+    local completionItem = capabilities.textDocument.completion.completionItem
+    completionItem.snippetSupport = if_nil(override.snippetSupport, true)
+    completionItem.preselectSupport = if_nil(override.preselectSupport, true)
+    completionItem.insertReplaceSupport = if_nil(override.insertReplaceSupport, true)
+    completionItem.labelDetailsSupport = if_nil(override.labelDetailsSupport, true)
+    completionItem.deprecatedSupport = if_nil(override.deprecatedSupport, true)
+    completionItem.commitCharactersSupport = if_nil(override.commitCharactersSupport, true)
+    completionItem.tagSupport = if_nil(override.tagSupport, { valueSet = { 1 } })
+    completionItem.resolveSupport = if_nil(override.resolveSupport, {
+        properties = {
+            'documentation',
+            'detail',
+            'additionalTextEdits',
+        }
+    })
+    return capabilities
+end
+
 -- nvim-cmp supports additional completion capabilities
--- local capabilities = vim.lsp.protocol.make_client_capabilities()
--- capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = update_capabilities(capabilities)
 
 -- For pyright, try to adapt for venv
 local util = require('lspconfig/util')
