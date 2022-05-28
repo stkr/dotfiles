c4d336d3c958a27dc13fb8900a8eab576fb0a1a7
stkr <stkr@users.noreply.github.com>
2022-05-28 19:25:34 +0200

    Add alias for quickly opening "nvim project files".

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     alias dt='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+    alias np='nvim -S .project.vim'
 
     # Use ctrl+l to complete one word of autosuggestion
     bind -M insert \cl 'forward-word'
