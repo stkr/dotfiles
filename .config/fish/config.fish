2c6f3c59df044d93cc6bc60caddb8981aadcc64e
stkr <stkr@users.noreply.github.com>
2022-06-06 17:30:07 +0200

    Add alias for viewing thumbnails of images in a dir.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     alias dt='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
     alias np='nvim -S .project.vim'
+    alias thumbs='feh --thumbnails --thumb-width=200 --thumb-height=200 --limit-width=1900'
 
     # Use ctrl+l to complete one word of autosuggestion
     bind -M insert \cl 'forward-word'
