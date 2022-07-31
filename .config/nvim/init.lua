8b612c60bdd2bfbb3461594695dd4f5be3370ffc
stkr <stkr@users.noreply.github.com>
2022-07-31 12:32:14 +0200

    Disable default lsp mappings (confilcts!).

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -544,25 +544,25 @@ require('nvim-treesitter.configs').setup {
 
 -- LSP settings
 local lspconfig = require 'lspconfig'
-local on_attach = function(_, bufnr)
-    local opts = { buffer = bufnr }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
-    vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
-    vim.keymap.set('n', '<leader>wl', function()
-        vim.inspect(vim.lsp.buf.list_workspace_folders())
-    end, opts)
-    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
-    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', '<leader>so', require('telescope.builtin').lsp_document_symbols, opts)
-    vim.api.nvim_create_user_command("Format", vim.lsp.buf.formatting, {})
-end
+-- local on_attach = function(_, bufnr)
+--     -- local opts = { buffer = bufnr }
+--     -- vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+--     -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+--     -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+--     -- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+--     -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+--     -- vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+--     -- vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+--     -- vim.keymap.set('n', '<leader>wl', function()
+--     --     vim.inspect(vim.lsp.buf.list_workspace_folders())
+--     -- end, opts)
+--     -- vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+--     -- vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+--     -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+--     -- vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+--     -- vim.keymap.set('n', '<leader>so', require('telescope.builtin').lsp_document_symbols, opts)
+--     -- vim.api.nvim_create_user_command("Format", vim.lsp.buf.formatting, {})
+-- end
 
 -- This is a copy from cmp_nvim_lsp/init.lua. The rationale here is that in order
 -- to use cmp_nvim_lsp one would need to require nvim-cmp in addition. However, we
