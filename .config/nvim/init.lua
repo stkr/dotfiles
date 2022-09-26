aedc5965d190161adc0603e7a41ffc7bd83cb1b9
stkr <stkr@users.noreply.github.com>
2022-09-26 19:38:25 +0200

    Improve keymapping for Hop.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -519,8 +519,7 @@ vim.keymap.set('', 'f', function() require("hop").hint_char1({ direction = requi
 vim.keymap.set('', 'F', function() require("hop").hint_char1({ direction = require("hop.hint").HintDirection.BEFORE_CURSOR, current_line_only = true }) end, {})
 vim.keymap.set('', 't', function() require("hop").hint_char1({ direction = require("hop.hint").HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 }) end, {})
 vim.keymap.set('', 'T', function() require("hop").hint_char1({ direction = require("hop.hint").HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 }) end, {})
-vim.keymap.set('', '\\', function() require("hop").hint_char2({ direction = require("hop.hint").HintDirection.AFTER_CURSOR }) end, {})
-vim.keymap.set('', '|', function() require("hop").hint_char2({ direction = require("hop.hint").HintDirection.BEFORE_CURSOR }) end, {})
+vim.keymap.set('', 'ss', function() require("hop").hint_char2() end, {})
 
 --#region autocommands
 
