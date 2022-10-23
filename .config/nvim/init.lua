d5eb8a055b917dd8eaf393b6f514b76818b2a162
stkr <stkr@users.noreply.github.com>
2022-10-23 11:21:54 +0200

    Move function for python venv detection to utils.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -780,30 +780,9 @@ capabilities = vim.tbl_extend('keep', capabilities or {}, lsp_status.capabilitie
 
 
 -- For pyright, try to adapt for venv
-local util = require('lspconfig/util')
-local path = util.path
-
-local function get_python_path(workspace)
-    -- Use activated virtualenv.
-    if vim.env.VIRTUAL_ENV then
-        return path.join(vim.env.VIRTUAL_ENV, 'bin', 'python')
-    end
-
-    -- Find and use virtualenv in workspace directory.
-    for _, pattern in ipairs({ '*', '.*' }) do
-        local match = vim.fn.glob(path.join(workspace, pattern, 'pyvenv.cfg'))
-        if match ~= '' then
-            return path.join(path.dirname(match), 'bin', 'python')
-        end
-    end
-
-    -- Fallback to system Python.
-    return vim.fn.exepath('python3') or vim.fn.exepath('python') or 'python'
-end
-
 lspconfig.pyright.setup({
     before_init = function(_, config)
-        config.settings.python.pythonPath = get_python_path(config.root_dir)
+        config.settings.python.pythonPath = utils.get_python_path(config.root_dir)
     end,
     on_attach = on_attach,
     capabilities = capabilities,
