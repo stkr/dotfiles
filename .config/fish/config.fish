3b3aa314e67487488c7bb6c576b3bccfeb4a6414
stkr <stkr@users.noreply.github.com>
2023-06-01 09:30:31 +0200

    Add fzf and fzf git keybindings for fish.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,19 @@ if status is-interactive
     alias np='nvim -S .project.vim'
     alias thumbs='feh --thumbnails --thumb-width=200 --thumb-height=200 --limit-width=1900'
 
+    # Load the fzf keybindings. Note, this relies on key-bindings.fish being
+    # installed in ~/.config/fish/functions!
+    if test -f ~/.config/fish/functions/key-bindings.fish
+        fzf_key_bindings
+    end
+
+    # Load the fzf keybindings. Note, this relies on git_fzf.fish being
+    # installed in ~/.config/fish/functions!
+    if test -f ~/.config/fish/functions/git_fzf.fish
+        source ~/.config/fish/functions/git_fzf.fish
+        git_fzf_key_bindings
+    end
+
     # Use ctrl+l to complete one word of autosuggestion
     bind -M insert \cl 'forward-word'
 end
