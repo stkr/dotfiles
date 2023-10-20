531f78b28e87a1670baac3db5c0015287a9a60a5
stkr <stkr@users.noreply.github.com>
2023-10-20 10:41:19 +0200

    Keybindings and sane options for nvim-tree.

--- .config/nvim/lua/config/nvim-tree.lua
+++ .config/nvim/lua/config/nvim-tree.lua
@@ -6,9 +6,38 @@ end
 
 local callbacks = {}
 
+local function on_attach_callback(bufnr)
+    local api = require("nvim-tree.api")
+
+    local function opts(desc)
+        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    end
+
+    -- default mappings
+    api.config.mappings.default_on_attach(bufnr)
+
+    -- custom mappings
+    vim.keymap.set("n", "l", api.node.open.edit(),  opts("Edit Or Open"))
+    vim.keymap.set("n", "h", api.tree.close,        opts("Close"))
+    vim.keymap.set("n", "H", api.tree.collapse_all, opts("Collapse All"))
+end
+
 function callbacks.config()
     tree.setup({
+        actions = {
+            open_file = {
+                resize_window = false,
+            },
+        },
+        live_filter = {
+            prefix = "[S]: ",
+            always_show_folders = false,
+        },
+        on_attach = on_attach_callback,
         sync_root_with_cwd = true,
+        view = {
+            preserve_window_proportions = true,
+        },
     })
 end
 
