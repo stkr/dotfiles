00fbaad584bcbcd000fa1c4b93d80d0f5583b24b
stkr <stkr@users.noreply.github.com>
2023-11-04 22:46:23 +0100

    Split-off treesitter-textobjects plugin.

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,11 @@
 return
 {
     'nvim-treesitter/nvim-treesitter',
-    dependencies = {
-        { 'nvim-treesitter/nvim-treesitter-textobjects' },
-    },
     config = function()
-        -- Treesitter configuration
-        -- Parsers must be installed manually via :TSInstall
+        require("nvim-treesitter.install").prefer_git = true,
         require('nvim-treesitter.configs').setup {
+            ensure_installed = {"c", "python", "rust", "lua", "vim", "vimdoc", "query" },
+            auto_install = false,
             highlight = {
                 enable = true,
                 disable = function(lang, buf)
@@ -30,39 +28,10 @@ return
             indent = {
                 enable = true,
             },
-            textobjects = {
-                select = {
-                    enable = true,
-                    lookahead = true,     -- Automatically jump forward to textobj, similar to targets.vim
-                    keymaps = {
-                        -- You can use the capture groups defined in textobjects.scm
-                        ['af'] = '@function.outer',
-                        ['if'] = '@function.inner',
-                        ['ac'] = '@class.outer',
-                        ['ic'] = '@class.inner',
-                    },
-                },
-                move = {
-                    enable = true,
-                    set_jumps = true,     -- whether to set jumps in the jumplist
-                    goto_next_start = {
-                        [']m'] = '@function.outer',
-                        [']]'] = '@class.outer',
-                    },
-                    goto_next_end = {
-                        [']M'] = '@function.outer',
-                        [']['] = '@class.outer',
-                    },
-                    goto_previous_start = {
-                        ['[m'] = '@function.outer',
-                        ['[['] = '@class.outer',
-                    },
-                    goto_previous_end = {
-                        ['[M'] = '@function.outer',
-                        ['[]'] = '@class.outer',
-                    },
-                },
-            },
         }
-    end
+    end,
+    build = ":TSUpdate",
+    dependencies = {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+    },
 }
