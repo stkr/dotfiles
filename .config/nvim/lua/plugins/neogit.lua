dbc85b0741e82be747ae14b9cedd29bf717eca6e
stkr <stkr@users.noreply.github.com>
2023-11-04 21:18:30 +0100

    Split-off neogit plugin.

---
+++ .config/nvim/lua/plugins/neogit.lua
@@ -0,0 +1,11 @@
+return
+{
+    "NeogitOrg/neogit",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope.nvim",
+        "sindrets/diffview.nvim",
+    },
+    cmd = { "Neogit" },
+    config = true
+}
