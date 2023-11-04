9725ac146cd9a6c8ab0af805bb4539c5fa8c6964
stkr <stkr@users.noreply.github.com>
2023-11-04 21:12:24 +0100

    Split-off gitgutter plugin.

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,15 +1,12 @@
 local utils = require("utils")
 
 -- In this file we keep all the essential/small plugins that require 
--- no configuration whatsowver and are pretty much used always.
+-- no configuration whatsoever and are pretty much used always.
 
 return {
 
     { "nvim-lua/plenary.nvim", },
 
-
-    -- Add git related info in the signs columns and popups
-    { "airblade/vim-gitgutter", },
 
     {
         "NeogitOrg/neogit",
