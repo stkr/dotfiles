3800a5e215135f9c0042393e74d9bb4b5167340f
stkr <stkr@users.noreply.github.com>
2023-11-05 07:21:18 +0100

    Split-off nvim-cmp plugin.

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -7,25 +7,6 @@ return {
 
     { "nvim-lua/plenary.nvim", },
 
-    -- autocompletion
-    {
-        "hrsh7th/nvim-cmp",
-        event = "InsertEnter",
-        dependencies = {
-
-            {
-                "L3MON4D3/LuaSnip",
-                dependencies = { { "rafamadriz/friendly-snippets", }, },
-            },
-
-            { "saadparwaiz1/cmp_luasnip", },
-            { "hrsh7th/cmp-nvim-lsp", },
-            { "hrsh7th/cmp-buffer", },
-            { "hrsh7th/cmp-path", },
-        },
-        config = function() require("config.nvim-cmp").config() end,
-    },
-
     {
         'nvim-telescope/telescope.nvim',
         cmd = { "Telescope" },
