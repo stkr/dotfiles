4ab9fb8b0cd2f2aa885c141dbfd8d59ea79bd5a8
stkr <stkr@users.noreply.github.com>
2023-11-05 13:56:30 +0100

    Disable cutlass plugin for now (seems to break which-key).

--- .config/nvim/lua/plugins/cutlass.lua
+++ .config/nvim/lua/plugins/cutlass.lua
@@ -1,6 +1,7 @@
 return
 {
     "gbprod/cutlass.nvim",
+    enabled = false, -- this plugin breaks which-key in visual mode apparently, need to find out more...
     config = function()
         require("cutlass").setup({
             exclude = { "ns", "nS" },
