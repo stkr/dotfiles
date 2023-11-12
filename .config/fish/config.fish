37ef144849c3ee4106d6aa62f60f0d5561e9c1e1
stkr <stkr@users.noreply.github.com>
2023-11-12 16:10:07 +0100

    Fix alias for opening session in nvim.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     alias dt='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-    alias np='nvim -S .project.vim'
+    alias np='nvim -S .session.vim'
     alias thumbs='feh --thumbnails --thumb-width=200 --thumb-height=200 --limit-width=1900'
 
     # Load the fzf keybindings. Note, this relies on key-bindings.fish being
