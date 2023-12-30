7cb4b53d4ac94d210305cb6cd5276f11b0da27ea
stkr <stkr@users.noreply.github.com>
2023-12-30 22:46:11 +0100

    Remap reformat map for gdscript.

--- .config/nvim/after/ftplugin/gdscript.lua
+++ .config/nvim/after/ftplugin/gdscript.lua
@@ -1,3 +1,13 @@
 vim.opt_local.tabstop = 4
 vim.opt_local.shiftwidth = 4
 vim.opt_local.expandtab = false
+
+local function format_file()
+    vim.cmd([[ 
+        write
+        silent ! gdformat % 
+    ]])
+end
+
+-- buffer = 0 makes it a buffer-local keymap (only applies to current (==0) buffer).
+vim.keymap.set('n', '<leader>rf', format_file, { desc = "Format file (gdformat)", buffer = 0 })
