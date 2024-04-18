e9d36156bd1fe2a25351241d84249c6ab9751545
stkr <stkr@users.noreply.github.com>
2024-04-18 14:46:45 +0200

    Disable (slow) previewer on quickfixhistory picker.

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -181,7 +181,7 @@ vim.keymap.set('n', '<leader>fnp',
         tk.find_notes()
     end, { desc = "Find note in category: persons" })
 
-vim.keymap.set('n', '<leader>fq', function() require('telescope.builtin').quickfixhistory() end,
+vim.keymap.set('n', '<leader>fq', function() require('telescope.builtin').quickfixhistory({previewer = false}) end,
     { desc = "Find quickfix history" })
 vim.keymap.set('n', '<leader>ft', function() require('telescope.builtin').tags() end, { desc = "Find tag" })
 vim.keymap.set('n', '<leader>f:', function() require('telescope.builtin').command_history() end,
