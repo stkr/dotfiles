f26e84f23819b383caac48cfdce657af396e4590
stkr <stkr@users.noreply.github.com>
2024-07-25 17:21:27 +0200

    Improve colors used in fzf-lua.

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -5,11 +5,11 @@ return
     dependencies = { "nvim-tree/nvim-web-devicons" },
     cmd = "FzfLua",
     opts = {
-        defaults = {
+        defaults   = {
             file_icons = true,
             git_icons = false, -- This is VERY slow on large repos in windows. Disabled for now.
         },
-        grep     = {
+        grep       = {
             rg_glob = true,
             -- first returned string is the new search query
             -- second returned string are (optional) additional rg flags
@@ -21,17 +21,32 @@ return
             end
 
         },
-        keymap   = {
+        keymap     = {
             fzf = {
                 ["ctrl-d"] = "half-page-down",
                 ["ctrl-u"] = "half-page-up",
                 ["ctrl-q"] = "select-all+accept",
             },
         },
-        winopts  = {
+        winopts    = {
             preview = {
                 default = false,
             },
+        },
+        fzf_colors = {
+            ["fg"]      = { "fg", "CursorLine" },
+            ["bg"]      = { "bg", "Normal" },
+            ["hl"]      = { "fg", "Comment" },
+            ["fg+"]     = { "fg", "Normal" },
+            ["bg+"]     = { "bg", "CursorLine" },
+            ["hl+"]     = { "fg", "Statement" },
+            ["info"]    = { "fg", "PreProc" },
+            ["prompt"]  = { "fg", "Conditional" },
+            ["pointer"] = { "fg", "Exception" },
+            ["marker"]  = { "fg", "Keyword" },
+            ["spinner"] = { "fg", "Label" },
+            ["header"]  = { "fg", "Comment" },
+            ["gutter"]  = "-1",
         },
     },
 }
