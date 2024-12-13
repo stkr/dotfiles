87e7fe5984b562b84637cd114822d8d6da0ebfb1
stkr <stkr@users.noreply.github.com>
2024-12-13 12:47:44 +0100

    Fix keymaps and pin version for copyright updater.

--- private_dot_config/nvim/lua/plugins/copyright-updater.lua
+++ private_dot_config/nvim/lua/plugins/copyright-updater.lua
@@ -1,10 +1,12 @@
 return
 {
     'LittleMorph/copyright-updater.nvim',
+    commit = "b5986fee560ae8981598ae4da8d4c949a0550da0",
     event = 'BufModifiedSet', -- Delay loading until a buffer is modified
     opts = {
+        return_cursor = true,
         mappings = {
-            toggle = nil,
+            toggle = "",
         },
         limiters = {
             range = '1,10',
