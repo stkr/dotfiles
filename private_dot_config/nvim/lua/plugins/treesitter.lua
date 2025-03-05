0c82e2c0e125bcbf9c46405a89525de706c1c682
stkr <stkr@users.noreply.github.com>
2025-03-05 12:25:26 +0100

    Update treesitter languages.

--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -3,9 +3,15 @@ return
     'nvim-treesitter/nvim-treesitter',
     version = "0.9.2",
     config = function()
-        require("nvim-treesitter.install").prefer_git = true,
+        require("nvim-treesitter.install").prefer_git = true
         require('nvim-treesitter.configs').setup {
-            ensure_installed = {"c", "python", "rust", "lua", "vim", "vimdoc", "query", "markdown", },
+            ensure_installed = { "bash", "c", "c_sharp", "cmake", "cpp", "css", "csv", "devicetree",
+                "dockerfile", "dot", "doxygen", "dtd", "fish", "gdscript", "git_config", "git_rebase",
+                "gitattributes", "gitcommit", "gitignore", "godot_resource", "gpg", "html", "http",
+                "java", "javascript", "json5", "kconfig", "kotlin", "lua", "luap", "make", "markdown",
+                "markdown_inline", "matlab", "ninja", "objdump", "pem", "perl", "proto", "python",
+                "regex", "rst", "ruby", "rust", "sql", "ssh_config", "toml", "verilog",
+                "vim", "vimdoc", "xml", "yaml", },
             auto_install = false,
             highlight = {
                 enable = true,
@@ -30,7 +36,7 @@ return
                 enable = true,
                 disable = {
                     -- https://www.reddit.com/r/neovim/comments/1agynre/how_to_make_markdown_list_items_indent_behind_the/
-                    "markdown",  -- indentation at bullet points is worse than native neovim indentation
+                    "markdown",     -- indentation at bullet points is worse than native neovim indentation
                 },
             },
         }
