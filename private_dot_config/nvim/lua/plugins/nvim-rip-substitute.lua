d083b9a72e099b06165114a02c1fb85a713db8dc
stkr <stkr@users.noreply.github.com>
2025-03-07 17:06:12 +0100

    Add nvim-rip-substitute and adapt keymaps accordingly.

---
+++ private_dot_config/nvim/lua/plugins/nvim-rip-substitute.lua
@@ -0,0 +1,11 @@
+return
+{
+    "chrisgrieser/nvim-rip-substitute",
+    cmd = "RipSubstitute",
+    opts = {
+        regexOptions = {
+            pcre2 = false,
+        },
+    },
+    lazy = true,
+}
