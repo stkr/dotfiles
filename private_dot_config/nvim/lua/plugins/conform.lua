f182ab95a6f27af23e69666dc5628e027841c43a
stkr <stkr@users.noreply.github.com>
2025-08-05 20:04:24 +0200

    Add format on save.

--- private_dot_config/nvim/lua/plugins/conform.lua
+++ private_dot_config/nvim/lua/plugins/conform.lua
@@ -1,23 +1,27 @@
 return
 {
     'stevearc/conform.nvim',
-    -- lazy = true,
-    opts = {
-        -- log_level = vim.log.levels.DEBUG,
-        formatters_by_ft = {
-            -- Use the "*" filetype to run formatters on all filetypes.
-            ["*"] = { "trim_whitespace", "trim_newlines" },
-            -- Use the "_" filetype to run formatters on filetypes that don't
-            -- have other formatters configured.
-            ["_"] = {  },
-        },
-        default_format_opts = {
-            lsp_format = "fallback",
-        },
-        format_on_save = {
-            -- I recommend these options. See :help conform.format for details.
-            lsp_format = "fallback",
-            timeout_ms = 500,
-        },
-    },
+    config = function()
+        local opts = {
+            -- log_level = vim.log.levels.DEBUG,
+            formatters_by_ft = {
+                -- Use the "*" filetype to run formatters on all filetypes.
+                ["*"] = { "trim_whitespace", "trim_newlines" },
+                -- Use the "_" filetype to run formatters on filetypes that don't
+                -- have other formatters configured.
+                ["_"] = {},
+            },
+            default_format_opts = {
+                lsp_format = "fallback",
+            },
+            format_on_save = function(bufnr)
+                -- Disable with a global or buffer-local variable
+                if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+                    return
+                end
+                return { timeout_ms = 500, lsp_format = "fallback" }
+            end,
+        }
+        require("conform").setup(opts)
+    end
 }
