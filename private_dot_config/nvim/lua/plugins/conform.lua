5a176fa30430a6e801ac960a65c6d691a45a23b7
stkr <stkr@users.noreply.github.com>
2025-08-05 19:48:21 +0200

    Add whitespace trimming for all files.

--- private_dot_config/nvim/lua/plugins/conform.lua
+++ private_dot_config/nvim/lua/plugins/conform.lua
@@ -1,11 +1,23 @@
 return
 {
     'stevearc/conform.nvim',
-    lazy = true,
+    -- lazy = true,
     opts = {
         -- log_level = vim.log.levels.DEBUG,
+        formatters_by_ft = {
+            -- Use the "*" filetype to run formatters on all filetypes.
+            ["*"] = { "trim_whitespace", "trim_newlines" },
+            -- Use the "_" filetype to run formatters on filetypes that don't
+            -- have other formatters configured.
+            ["_"] = {  },
+        },
         default_format_opts = {
             lsp_format = "fallback",
+        },
+        format_on_save = {
+            -- I recommend these options. See :help conform.format for details.
+            lsp_format = "fallback",
+            timeout_ms = 500,
         },
     },
 }
