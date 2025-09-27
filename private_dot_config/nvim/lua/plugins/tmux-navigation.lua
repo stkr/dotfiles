bd768064cecab4d8d2734f217d4b10571bcd50d9
stkr <stkr@users.noreply.github.com>
2025-09-27 20:38:42 +0200

    Add seamles pane navication between tmux and nvim.

---
+++ private_dot_config/nvim/lua/plugins/tmux-navigation.lua
@@ -0,0 +1,35 @@
+return {
+    "alexghergh/nvim-tmux-navigation",
+    config = function()
+        require("nvim-tmux-navigation").setup({
+            disable_when_zoomed = true, -- defaults to false
+            keybindings = {
+                left = "<M-h>",
+                down = "<M-j>",
+                up = "<M-k>",
+                right = "<M-l>",
+            },
+        })
+
+        local function tmux_command(command)
+            local tmux_socket = vim.fn.split(vim.env.TMUX, ",")[1]
+            return vim.fn.system("tmux -S " .. tmux_socket .. " " .. command)
+        end
+
+        local nvim_tmux_nav_group = vim.api.nvim_create_augroup("NvimTmuxNavigation", {})
+
+        vim.api.nvim_create_autocmd({ "VimEnter", "VimResume" }, {
+            group = nvim_tmux_nav_group,
+            callback = function()
+                tmux_command("set-option -p @is_vim yes")
+            end,
+        })
+
+        vim.api.nvim_create_autocmd({ "VimLeave", "VimSuspend" }, {
+            group = nvim_tmux_nav_group,
+            callback = function()
+                tmux_command("set-option -p -u @is_vim")
+            end,
+        })
+    end,
+}
