2e27a8d7ba567e6c1250b330b1d30bef99bb9631
stkr <stkr@users.noreply.github.com>
2026-03-11 13:06:45 +0000

    Experiments with dap.

--- private_dot_config/nvim/lua/plugins/dap.lua
+++ private_dot_config/nvim/lua/plugins/dap.lua
@@ -3,6 +3,23 @@ return
     "mfussenegger/nvim-dap",
     lazy = true,
     config = function()
+        local dap = require("dap")
+
+        dap.adapters.gdb = {
+            type = "executable",
+            command = "gdb",
+            args = { "--quiet",
+                    "--interpreter=dap",
+                    "--command=/home/nxp29037/projects/s1xy/debug/.gdbinit.bak",
+            }
+        }
+        dap.adapters.cppdbg = {
+
+          id = 'cppdbg',
+          type = 'executable',
+          command = '/home/nxp29037/.vscode/extensions/ms-vscode.cpptools-1.30.5-linux-x64/debugAdapters/bin/OpenDebugAD7',
+        }
+
         local utils = require("utils")
         utils.safe_require("dapui")
     end
