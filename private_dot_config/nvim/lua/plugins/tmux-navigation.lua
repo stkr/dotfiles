cc2e5e7d343d221c0e3952d9359d7377bed4c28f
stkr <stkr@users.noreply.github.com>
2026-03-19 12:16:30 +0000

    Extend tmux pane navigation to vifm.

--- private_dot_config/nvim/lua/plugins/tmux-navigation.lua
+++ private_dot_config/nvim/lua/plugins/tmux-navigation.lua
@@ -10,26 +10,5 @@ return {
                 right = "<M-l>",
             },
         })
-
-        local function tmux_command(command)
-            local tmux_socket = vim.fn.split(vim.env.TMUX, ",")[1]
-            return vim.fn.system("tmux -S " .. tmux_socket .. " " .. command)
-        end
-
-        local nvim_tmux_nav_group = vim.api.nvim_create_augroup("NvimTmuxNavigation", {})
-
-        vim.api.nvim_create_autocmd({ "VimEnter", "VimResume" }, {
-            group = nvim_tmux_nav_group,
-            callback = function()
-                tmux_command("set-option -p @is_vim yes")
-            end,
-        })
-
-        vim.api.nvim_create_autocmd({ "VimLeave", "VimSuspend" }, {
-            group = nvim_tmux_nav_group,
-            callback = function()
-                tmux_command("set-option -p -u @is_vim")
-            end,
-        })
-    end,
+    end
 }
