6c785f5db278dd93a0251735ef87fd5c26d7a9fa
stkr <stkr@users.noreply.github.com>
2026-04-09 10:49:04 +0100

    Use nice colors for BlameToggle.
    Somehow the random colors used by blame broke, and it was unreadable.
    This fixes this by using the catppuccin colors from the colorscheme
    instead.

--- private_dot_config/nvim/lua/plugins/blame.lua
+++ private_dot_config/nvim/lua/plugins/blame.lua
@@ -2,6 +2,24 @@ return {
     "FabijanZulj/blame.nvim",
     lazy = false,
     config = function()
-        require('blame').setup {}
+        local utils = require("utils")
+        local palette = require("catppuccin.palettes").get_palette("latte")
+        -- remove a few colors:
+        palette['base'] = nil
+        palette['subtext0'] = nil
+        palette['subtext1'] = nil
+        palette['surface0'] = nil
+        palette['surface1'] = nil
+        palette['surface2'] = nil
+        palette['crust'] = nil
+        palette['mantle'] = nil
+        palette['overlay0'] = nil
+        palette['overlay1'] = nil
+        palette['overlay2'] = nil
+        palette['yellow'] = nil
+        local colors = utils.values(palette)
+        require('blame').setup {
+            colors = colors,
+        }
     end,
 }
