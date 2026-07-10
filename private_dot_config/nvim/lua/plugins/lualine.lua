14c3c3991dd47847e2de3e77241b644be4529c5c
stkr <stkr@users.noreply.github.com>
2026-07-10 10:49:44 +0100

    Add overseer icon to lualine.

--- private_dot_config/nvim/lua/plugins/lualine.lua
+++ private_dot_config/nvim/lua/plugins/lualine.lua
@@ -59,7 +59,7 @@ return
             lualine_a = { "mode", "vim.o.paste and 'PASTE' or ''" },
             lualine_b = { "branch", "diff", "diagnostics", },
             lualine_c = { "filename", lsp_progress },
-            lualine_x = { "encoding", "fileformat", "filetype" },
+            lualine_x = { "overseer", "encoding", "fileformat", "filetype" },
             lualine_y = { "progress" },
             lualine_z = { "location" }
         },
